d937193fe7401f70a13026703389fba030fff4a8
Siduduziwe Mswabuki <siduduziwe.mswabuki@kcl.ac.uk>
2025-12-30 16:02:17 +0000

    Add git hub deployment scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://Dudu-M.github.io',
+  base: '/bh-company-site',
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  compressHTML: true,
 });
